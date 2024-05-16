43c5cb86b089cc034e06f51dbf84cf587133e7be
Andino <diego.andino93@gmail.com>
2024-05-16 14:32:27 -0600

    :sparkles: created the pages for the list and the form

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,18 +1,26 @@
-// app/page.js
 'use client';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { clearForm, setProps } from '@/store/form/reducer';
+import { FormComponent } from '@/components/registerForm';
+import { FormState } from "@/store/form/reducer";
 
 export default function FormPage() {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.form);
+  const count = useSelector((state:any) => state.form.currentUser);
+
+  const handleUpdate = (data: Partial<FormState>) => dispatch(setProps(data));
+  const handleClear = () => {
+    dispatch(clearForm());
+  }
 
   return (
     <div>
-      <h1>Count: {count.name}</h1>
-      <button onClick={() => dispatch(clearForm())}>Clear</button>
-      <button onClick={() => dispatch(setProps({name:"test"}))}>default</button>
+      <FormComponent
+        values={count}
+        updateActions={handleUpdate}
+        clearAction={handleClear}
+      />
     </div>
   );
 }
